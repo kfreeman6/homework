355aa08b656bb985951c755470e25d104e00c19f
vagrant <vagrant@kellogg-dev-box>
2014-04-21 23:27:57 +0000

    part 3 complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140421230819) do
+ActiveRecord::Schema.define(version: 20140421231954) do
+
+  create_table "books", force: true do |t|
+    t.string "title"
+    t.string "author"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
-
-
-Book.create(:title => "A Brief History of Time", :author => "Stephen Hawking")
-Book.create(:title => "Alice in Wonderland", :author => "Lewis Carroll")
-Book.create(:title => "The Lord Of The Rings", :author => "J.R.R. Tolkien")
-Book.create(:title => "The Hobbit", :author => "J.R.R. Tolkien")
+Book.create("title" => "A Brief History of Time", "author" => "Stephen Hawking")
+Book.create("title" => "Alice in Wonderland", "author" => "Lewis Carroll")
+Book.create("title" => "The Lord Of The Rings", "author" => "J.R.R. Tolkien")
+Book.create("title" => "The Hobbit", "author" => "J.R.R. Tolkien")
